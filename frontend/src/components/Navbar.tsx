8ab312c7ea64dadff05c7f40bab775132afd72ec
manuhutu344 <nandoperdos2@gmail.com>
2024-12-28 17:00:32 +0900

    Membuat Navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+import {Link, useNavigate} from "react-router-dom"
+import {useAuth} from "@clerk/clerk-react"
+import {FaBars, FaTimes} from "react-icons/fa"
+
+function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
