758d1fb065f337157a1e97456fdb8cc6ade64a16
manuhutu344 <nandoperdos2@gmail.com>
2024-12-28 17:30:05 +0900

    Memunculkan logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
+import Navbar from "./components/Navbar"
+import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom"
 
 
 function App() {
 
   return (
-    <>
-      Halooo
-    </>
+    <Router>
+      <Routes>
+        <Route index element={<Navbar />} />
+      </Routes>
+    </Router>
   )
 }
 
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,11 +1,26 @@
 import { useState } from "react"
 import {Link, useNavigate} from "react-router-dom"
-import {useAuth} from "@clerk/clerk-react"
+// import {useAuth} from "@clerk/clerk-react"
 import {FaBars, FaTimes} from "react-icons/fa"
+import styles from "../styles/NavbarStyles"
+import { logo } from "../assets/index"
 
 function Navbar() {
+    const navigation = useNavigate()
+    // const {isSignedIn} = useAuth()
+    const [isMenuOpen, setIsMenuOpen ] = useState(false)
+    const [isLogoLoaded, setIsLogoLoaded] = useState(true)
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen)
   return (
-    <div>Navbar</div>
+  <nav className={styles.navbar}>
+    <div className={styles.container}>
+        <Link to={"/"} className={styles.logo}>
+            {isLogoLoaded ? (<img src={logo} alt="Logo" onError={()=>setIsLogoLoaded(false)} className="w-16 h-16 md:w-20 md:h-20" />) : (
+                "Logo"
+            )}
+        </Link>
+    </div>
+  </nav>
   )
 }
 
