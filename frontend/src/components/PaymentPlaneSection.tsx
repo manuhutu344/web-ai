99da5c11d9286abf681d333532eef3e44a2ad29b
manuhutu344 <nandoperdos2@gmail.com>
2025-01-02 11:12:44 +0900

    masih error untuk pembayaran

--- frontend/src/components/PaymentPlaneSection.tsx
+++ frontend/src/components/PaymentPlaneSection.tsx
@@ -9,9 +9,50 @@ function PaymentPlaneSection() {
         {
             name: 'Coba',
             amount: 1500,
-            
-        }
+            description: "Coba Saja Baru Coba",
+            features: [
+              'Project Slot Hanya Satu',
+              'Analisis Basic',
+              'Support Email'
+            ]
+        },
+        {
+          name: 'Coba 1',
+          amount: 2500,
+          description: "Coba Saja Baru Coba",
+          features: [
+            'Project Slot Hanya Satu',
+            'Analisis Basic',
+            'Support Email'
+          ]
+      },
+      {
+        name: 'Coba 2',
+        amount: 3500,
+        description: "Coba Saja Baru Coba",
+        features: [
+          'Project Slot Hanya Satu',
+          'Analisis Basic',
+          'Support Email'
+        ]
+    }
     ]
+    async function handleCheckout(plan){
+      const stripe = await stripePromise
+      if(!stripe){
+        console.error('Stripe gagal muncul')
+        return
+      }
+      try {
+        const response = await axios.post('Dari Backend'),{
+          planId: plan.name,
+          planAmount: plan.amount,
+          planCurrency: 'idr',
+        }
+      } catch (error) {
+        
+      }
+    }
   return (
     <div>PaymentPlaneSection</div>
   )
