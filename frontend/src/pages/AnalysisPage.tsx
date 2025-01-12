7af1dc701d3a57d2d20635225a803b82091a2ce1
manuhutu344 <nandoperdos2@gmail.com>
2025-01-12 14:09:13 +0900

    membuat dashboard untuk page analisis

--- frontend/src/pages/AnalysisPage.tsx
+++ frontend/src/pages/AnalysisPage.tsx
@@ -1,6 +1,29 @@
+import { useNavigate } from "react-router-dom"
+import {prof, tube1, tube2, tube4, tube5} from "../assets/index"
+import styles from "../styles/AnalysisPageStyles"
+
 function AnalysisPage() {
+  const navigate = useNavigate()
+  function handleButtonClick(){
+    navigate("/dashboard/photo")
+  }
   return (
-    <div>AnalysisPage</div>
+    <div className={styles.container}>
+      <div className={styles.backgroundCircles}>
+        <div className={styles.circle1}></div>
+        <div className={styles.circle2}></div>
+      </div>
+      <div className={styles.container}>
+          <img src={tube1} alt="Tube 1" className={styles.tube1} />
+          <img src={tube2} alt="Tube 2" className={styles.tube2} />
+          <img src={tube4} alt="Tube 4" className={styles.tube3} />
+          <img src={tube5} alt="Tube 5" className={styles.tube4} />
+      </div>
+      <img src={prof} alt="Prof" className={styles.scientistImage} />
+      <button onClick={handleButtonClick} className={styles.startButton}>
+        Mulai Analysis
+      </button>
+    </div>
   )
 }
 
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,6 +1,62 @@
+import { useState, useRef, useEffect } from "react"
+import { Link, Outlet, useLocation } from "react-router-dom"
+import {FaHome, FaCamera, FaChartLine, FaHistory, FaBars} from "react-icons/fa"
+import { useUser } from "@clerk/clerk-react"
+import {logo, hair} from "../assets/index"
+import style from "../styles/DashboardStyles"
+
 function Dashboard() {
-  return (
-    <div>Dashboard</div>
+  const location = useLocation()
+  const isHomePage = location.pathname === "/"
+  const {user} = useUser()
+  const [showSidebar, setShowSidebar] = useState(false)
+  const [showCard, setShowCard] = useState(false)
+  const cardRef = useRef<HTMLDivElement>(null)
+  const toggleSidebar = () => setShowSidebar(!showSidebar)
+  const toggleCard = () => setShowCard((prev) => !prev)
+  useEffect(()=>{
+    const handleClickOutside = (event: MouseEvent) =>{
+      if(cardRef.current && !cardRef.current.contains(event.target as Node)) {
+        setShowCard(false)
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside)
+    return () => document.removeEventListener("mousedown", handleClickOutside)
+  }, [])
+  const maskeEmail = (email: string) => {
+    const [username, domain] = email.split("@")
+    const maskedUsername = username.length > 3 ? `${username.slice(0, 3)}**` : username
+    const maskedDomain = domain.split(".").map((part, index)=>(index === 0 ? `${part[0]}**` : part)).join('.')
+  }
+return (
+   <div className={style.container}>
+      {!isHomePage && (
+        <div className={style.mobileToggle}>
+          <span className={style.mobileTitle}>Analisis Rambut</span>
+          <button onClick={toggleSidebar} className={style.toggleButton}>
+              <FaBars className={style.toggleIcon} />
+          </button>
+        </div>
+      )}
+      {!isHomePage &&(
+        <aside className={`${style.sidebar} ${showSidebar ? "translate-x-0": "-translate-x-full"} md:translate-x-0`}>
+         <div className={style.logoContainer}>
+          <img src={logo} alt="Logo"  className={style.logoImage}/>
+         <span className={style.logoText}>Analisis Rambut</span>
+       </div>
+       <nav className={style.nav}>
+        <Link to={"/dashboard/analysis"} className={`${style.navLink} ${location.pathname === "/dashboard/analysis" ? style.activeLink : ""}`}>
+          <FaChartLine className={style.navIcon} />
+          <span className={style.navText}>Cantik</span>
+        </Link>
+        <Link to={"/dashboard/photo"} className={`${style.navLink} ${location.pathname === "/dashboard/photo" ? style.activeLink : ""}`}>
+          <FaCamera className={style.navIcon} />
+          <span className={style.navText}>Ambil Foto</span>
+        </Link>
+       </nav>
+       </aside>
+      )}
+   </div>
   )
 }
 
