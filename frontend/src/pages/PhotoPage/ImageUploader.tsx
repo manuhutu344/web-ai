2952fa1716fdf55b7c1c187ceb23cc6e8cf521f4
manuhutu344 <nandoperdos2@gmail.com>
2025-01-18 22:44:06 +0900

    membuat untuk foto rambut

--- frontend/src/pages/PhotoPage/ImageUploader.tsx
+++ frontend/src/pages/PhotoPage/ImageUploader.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom"
 import {alibaba, amazon, jumia} from "../../assets/index"
 import styles from "../../styles/ImageUploaderStyles"
 import UpgradeCard from "../UpgradeCard"
+import { div } from "three/tsl"
 
 interface Props{
     className: string,
@@ -54,10 +55,45 @@ function ImageUploader({imageSrc, setImageSrc, predictions, classifyImage, setIs
             case "Amazon":
                 url = `https://www.amazon.com/s?k=${selectedWebsite}`
                 break;
+            case "Alibaba":
+                url: `https://www.alibaba.com/trade/search?sb=y&IndexArea=product_en&searchText=${selectedWebsite}`
+                break;
+            case "Jumia":
+                url: `https://group.jumia.com.ng/catalog/?g=${selectedWebsite}`
+                break;
+                default:
+                    return;
         }
+        window.open(url, '_blank')
+        setShowModel(false)
     }
   return (
-    <div>ImageUploader</div>
+    <div className={styles.container}>
+        {credits < 1 ? (
+            <UpgradeCard />
+        ):(
+            <>
+            {imageSrc ? (
+                <img src={imageSrc} alt="Capured" className={styles.image} />
+            ):(
+                <img src="https://ecamle.com" alt="Masukan Foto" className={styles.image} />
+            )}
+            <h2 className={styles.title}>Ambil Foto Anda</h2>
+            <p className={styles.description}>Untuk Kita Analisis Lebih Lanjut</p>
+            <p className={styles.credits}>Credits: {credits}</p>
+            <button className={styles.button} onClick={()=>{
+                setIsCameraView(true)
+                setCredits(prev =>Math.max(prev -25, 0))
+            }} disabled={credits < 1}>
+                Pake Kamera
+            </button>
+            <div className="flex justify-center space-x-4 mb-4">
+
+            </div>
+            </>
+        )}
+
+    </div>
   )
 }
 
