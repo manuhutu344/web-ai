0ddbff0b3a4a40d55df54bfd3328450773f03182
manuhutu344 <nandoperdos2@gmail.com>
2025-01-23 22:37:26 +0900

    Selesai membuat prediksi image nya

--- frontend/src/pages/PhotoPage/PhotoPage.tsx
+++ frontend/src/pages/PhotoPage/PhotoPage.tsx
@@ -1,12 +1,43 @@
 import ImageUploader from "./ImageUploader"
 import CameraCapture from "./CameraCapture"
+import { useState, useEffect } from "react"
+import * as tmImage from "@teachablemachine/image"
 
 function PhotoPage() {
+  const [isCameraView, setIsCameraView] = useState(false)
+  const [imageSrc, setImageSrc] = useState<string | null>(null)
+  const [predictions, setPredictions] = useState<any | null>(null)
+  const [model, setModel] = useState<any | null>(null)
+  const modelURL = 'https://teachablemachine.withgoogle.com/models/vEdU4im2s/model.json'
+  const metadataURL = "https://teachablemachine.withgoogle.com/models/vEdU4im2s/metadata.json"
+   useEffect(()=>{
+    const loadModel = async()=>{
+      const loadModel = await tmImage.load(modelURL, metadataURL)
+      setModel(loadModel)
+    }
+    loadModel()
+   }, [])
+   async function classifyImage(file: File){
+    if(!model) return
+    const image = document.createElement('img')
+    image.src = URL.createObjectURL(file)
+    image.onload = async () =>{
+      const prediction = await model.predict(image)
+      setPredictions(prediction)
+      const predictionData = prediction.map((pred: any)=>({
+        className: pred.className,
+        probability: pred.probability
+      }))
+    }
+   }
   return (
-    <>
-    <ImageUploader />
-    <CameraCapture />
-    </>
+    <div className="min-h-screen bg-gardient-to-br from-gray-50 via-gray-100 to-gray-200 flex flex-col items-center justify-center">
+      {isCameraView ? (
+        <CameraCapture setImageSrc={setImageSrc} setIsCameraView={setIsCameraView} classifyImage={classifyImage} />
+      ):(
+        <ImageUploader imageSrc={imageSrc} setImageSrc={setImageSrc} predictions={predictions} classifyImage={classifyImage} setIsCameraView={setIsCameraView}  />
+      )}
+    </div>
   )
 }
 
--- frontend/src/pages/UpgradeCard.tsx
+++ frontend/src/pages/UpgradeCard.tsx
@@ -1,6 +1,18 @@
+import {Link} from "react-router-dom"
+
 function UpgradeCard() {
   return (
-    <div>UpgradeCard</div>
+    <div className="bg-red-100 border-red-500 text-red-700 p-44 rounded-lg mb-4">
+        <p className="font-bold">
+        Anda telah menyelesaikan semua kredit Anda
+        </p>
+        <p >
+        Harap tingkatkan untuk terus menggunakan fitur ini
+        </p>
+        <Link to={"/"} className="text-red-500 font-bold hover:underline">
+        Tingkatkan sekarang
+        </Link>
+    </div>
   )
 }
 
